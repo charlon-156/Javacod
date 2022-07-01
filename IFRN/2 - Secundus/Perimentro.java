02c45bc4f361a018dbd03c911ee17ad387cc6dc9
Charlon Fernandes <charlon135@gmail.com>
2022-07-01 14:23:15 -0300

    Operação Baruc pt2

--- IFRN/2 - Secundus/Perimentro.java
+++ IFRN/2 - Secundus/Perimentro.java
@@ -1,17 +1,19 @@
 package IFRN.Secundus;
 import java.util.Scanner;
+
 public class Perimentro {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         System.out.println("Programa para calcular o perimetro de um circulo");
         System.out.println("Primeiramente, me diga o valor de raio:");
         double raio = in.nextDouble();
-        System.out.println(" ");
         
         double pi = Math.PI;
         double perimetro = 2 * pi * raio;
 
         System.out.println("Valor do perimetro = "+perimetro);
+        
         in.close();
     }
 }
--- IFRN/2 - Secundus/Segundo.java
+++ IFRN/2 - Secundus/Segundo.java
@@ -1,17 +1,23 @@
 package IFRN.Secundus;
 import java.util.Scanner;
+
 public class Segundo {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
         System.out.println("digite os segundos: ");
         int segundos = in.nextInt();
+
         int hora = segundos/3600;
         segundos %= 3600;
+
         int minutos = segundos / 60;
         segundos %= 60;
+
         System.out.println("Horas.....:"+hora);
         System.out.println("Minutos...:"+minutos);
         System.out.println("Segundos..:"+segundos);
+        
         in.close();   
     }
 }
